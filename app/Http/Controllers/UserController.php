fdbab5bc2e2c326f559bf68ce6ea1fa02297ef23
ahmed126742 <64670413+ahmed126742@users.noreply.github.com>
2020-07-22 03:18:07 -0700

    commit to master

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,6 +11,23 @@
 
     class UserController extends Controller
     {
+            /**
+     * @SWG\Get(
+     *   path="/api/testing/{mytest}",
+     *   summary="Get Testing",
+     *   operationId="testing",
+     *   @SWG\Response(response=200, description="successful operation"),
+     *   @SWG\Response(response=406, description="not acceptable"),
+     *   @SWG\Response(response=500, description="internal server error"),
+	 *		@SWG\Parameter(
+     *          name="mytest",
+     *          in="path",
+     *          required=true, 
+     *          type="string" 
+     *      ),
+     * )
+     *
+     */
         public function authenticate(Request $request)
         {
             $credentials = $request->only('email', 'password');
